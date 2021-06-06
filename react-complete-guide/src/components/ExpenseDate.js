4936492747796af16883178730fffdaff4d27a0d
Szőke Viktor <viktor.szoke@schdesign.hu>
2021-06-06 16:12:29 +0200

    Add expense date component

---
+++ react-complete-guide/src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ExpenseDate.css';
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString('hu-HU', { month: 'long' });
+  const day = props.date.toLocaleString('hu-HU', { day: '2-digit' });
+  const year = props.date.getFullYear();
+  return (
+    <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate
