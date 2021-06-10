2f4c2fb58df84e61461923c7fd9befa7fbee6653
Szőke Viktor <viktor.szoke@schdesign.hu>
2021-06-10 20:50:19 +0200

    Filter expenses

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -15,11 +15,13 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onDropdownChange={filterChangeHandler} />
+        {/* Hard coded version */}
         {/* <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
       <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
       <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
     <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} /> */}
-        {props.expenses.map((expense) => (
+
+        {props.expenses.filter(x => x.date.getFullYear().toString() === filteredYear).map((expense) => (
           <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={expense.id} />
         ))}
       </Card>
