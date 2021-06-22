80e1d80bc1cfa06350c784ab3b4635267bfc54dd
Szőke Viktor <viktor.szoke@schdesign.hu>
2021-06-22 14:34:12 +0200

    Add Animation with useEffect

--- react-complete-guide/food-order-app/src/components/UI/HeaderCartButton.js
+++ react-complete-guide/food-order-app/src/components/UI/HeaderCartButton.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartContext = useContext(CartContext);
+  const { items } = cartContext;
 
-  const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    // Ez egy clean up függvény amivel visszatérünk
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
